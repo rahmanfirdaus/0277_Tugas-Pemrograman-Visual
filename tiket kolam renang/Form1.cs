1efef3d7c51eb4548ad906854c58ff735518d173
rahmanfirdaus <136960206+rahmanfirdaus@users.noreply.github.com>
2025-04-01 20:06:04 +0700

    Update Form1.cs

--- tiket kolam renang/Form1.cs
+++ tiket kolam renang/Form1.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
-namespace booking_tiket_bioskop
+namespace tiket_kolam_renang
 {
     public partial class Form1 : Form
     {
